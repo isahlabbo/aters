cca092594177d1b9e4f752b18ba2aa42f2ba2741
Abu Saleem <abusaleem>
2019-01-29 18:14:52 +0100

    create PollingUnitIncidence model

---
+++ database/migrations/2019_01_29_171240_create_polling_unit_incidences_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePollingUnitIncidencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('polling_unit_incidences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('polling_unit_incidences');
+    }
+}
