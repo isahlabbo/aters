d7a63f176b808952d5947a65eb5273c06f817bdd
Mudassir Adili <mudassiradili4u@gmail.com>
2019-02-14 15:43:09 +0100

    add chartjs cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
 </head>
 <body>
     <div id="app">
